515dd339a74cbf341528b7e241e2b58e31ef2704
libo1106@foxmail.com <>
2013-08-08 12:36:10 +0000

    添加数据库信息

---
+++ App/Conf/config.php
@@ -0,0 +1,15 @@
+<?php
+  $dbArray = json_decode(getenv("VCAP_SERVICES"),true);
+
+  return array(
+    //自动开启SESSION
+    'SESSION_AUTO_START' => true,
+
+    //数据库配置信息
+    'DB_TYPE'   => 'mysql', // 数据库类型
+    'DB_HOST'   => $dbArray['mysql-5.1']['credentials']['host'], // 服务器地址
+    'DB_NAME'   => $dbArray['mysql-5.1']['credentials']['name'], // 数据库名
+    'DB_USER'   => $dbArray['mysql-5.1']['credentials']['user'], // 用户名
+    'DB_PWD'    => $dbArray['mysql-5.1']['credentials']['password'], // 密码
+    'DB_PORT'   => $dbArray['mysql-5.1']['credentials']['port'], // 端口    
+  );
